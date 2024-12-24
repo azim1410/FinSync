87acbaaf30b412aa3d7de2e56adc32e7ac1c69aa
Azim Damani <azim_damani@epam.com>
2024-12-24 13:20:34 +0530

    signup done

--- frontend/src/assets/AppRoute.tsx
+++ frontend/src/assets/AppRoute.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../Pages/Login/Login';
+import Signup from '../Pages/Signup/Signup';
 
 const AppRoute = () => {
   return (
     <>
     <BrowserRouter>
     <Routes>
+        <Route path="/signup" element={<Signup />}/>
         <Route path="/login" element={<Login />}/>
     </Routes>
     </BrowserRouter>
