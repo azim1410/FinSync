58819aba464728f0c09e09ba768fa0803ca1eabe
samad mohammed <mohd.samad1603@gmail.com>
2024-12-25 02:36:00 +0530

    Navbar done

--- frontend/src/AppRoute.tsx
+++ frontend/src/AppRoute.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
+import Navbar from "./Components/Navbar/Navbar";
 
 const AppRoute = () => {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
