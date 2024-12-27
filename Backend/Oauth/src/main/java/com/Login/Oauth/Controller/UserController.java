f5a60a6e0c74dc521f08f1c4edcd978d50b1ce68
Azim Damani <azim_damani@epam.com>
2024-12-27 18:02:09 +0530

    UI changes -2

--- Backend/Oauth/src/main/java/com/Login/Oauth/Controller/UserController.java
+++ Backend/Oauth/src/main/java/com/Login/Oauth/Controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
     private UserService userService;
